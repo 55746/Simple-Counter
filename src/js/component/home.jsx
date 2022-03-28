0e21f348f3b17be3355fd61b2abe1b1672df81e1
55746 <Lawhead.gavin@live.jobcorps.org>
2022-03-28 18:40:41 +0000

    almost figured it out

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+// const Home = () => {
+// 	setInterval(() => {
+// 		alert(1);
+// 	}, 1000);
+// };
+// export default Home;
 
-//create your first component
 const Home = () => {
+	const [timer, setTimer] = useState(0);
+	useEffect(() => {
+		setInterval(() => {
+			setTimer((timer) => timer + 1);
+		}, 100000);
+	}, []);
+	// if (timer === 10) {
+	// 	setTimer(0);
+	// }
+	// const [timer2, setTimer2] = useState(0);
+	// useEffect(() => {
+	// 	setInterval(() => {
+	// 		setTimer2((timer) => timer + 1);
+	// 	}, 10000);
+	// }, []);
+	// if (timer2 === 10) {
+	// 	setTimer(0);
+	// }
+	// const [timer3, setTimer3] = useState(0);
+	// useEffect(() => {
+	// 	setInterval(() => {
+	// 		setTimer3((timer) => timer + 1);
+	// 	}, 1000);
+	// }, []);
+	// if (timer3 === 10) {
+	// 	setTimer(0);
+	// }
+	const [timer4, setTimer4] = useState(0);
+	useEffect(() => {
+		setInterval(() => {
+			setTimer4((timer) => timer + 1);
+		}, 100);
+	}, []);
+	// if (timer4 === 10) {
+	// 	setTimer(0);
+	// }
 	return (
 		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1>
+				{timer}
+				{/* {timer2} */}
+				{/* {timer3} */}
+				{timer4}
+			</h1>
 		</div>
 	);
 };
