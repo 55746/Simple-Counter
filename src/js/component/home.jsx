e56b5b235ff4c5ffe428f56cce0ab4f521b16c94
55746 <Lawhead.gavin@live.jobcorps.org>
2022-03-29 00:58:25 +0000

    unfinished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -12,45 +12,35 @@ const Home = () => {
 	useEffect(() => {
 		setInterval(() => {
 			setTimer((timer) => timer + 1);
-		}, 100000);
+		}, 100000000);
+		if (timer === 10) {
+			setTimer(0);
+		}
 	}, []);
-	// if (timer === 10) {
-	// 	setTimer(0);
-	// }
-	// const [timer2, setTimer2] = useState(0);
-	// useEffect(() => {
-	// 	setInterval(() => {
-	// 		setTimer2((timer) => timer + 1);
-	// 	}, 10000);
-	// }, []);
-	// if (timer2 === 10) {
-	// 	setTimer(0);
-	// }
-	// const [timer3, setTimer3] = useState(0);
-	// useEffect(() => {
-	// 	setInterval(() => {
-	// 		setTimer3((timer) => timer + 1);
-	// 	}, 1000);
-	// }, []);
-	// if (timer3 === 10) {
-	// 	setTimer(0);
-	// }
-	const [timer4, setTimer4] = useState(0);
+	const [timer2, setTimer2] = useState(0);
 	useEffect(() => {
 		setInterval(() => {
-			setTimer4((timer) => timer + 1);
-		}, 100);
+			setTimer2((timer) => timer + 1);
+		}, 100000);
+		if (timer2 === 10) {
+			setTimer2(0);
+		}
 	}, []);
-	// if (timer4 === 10) {
-	// 	setTimer(0);
-	// }
+	const [timer3, setTimer3] = useState(0);
+	useEffect(() => {
+		setInterval(() => {
+			setTimer3((timer) => timer + 1);
+		}, 1000);
+		if (timer3 === 10) {
+			setTimer3(0);
+		}
+	}, []);
 	return (
 		<div>
 			<h1>
 				{timer}
-				{/* {timer2} */}
-				{/* {timer3} */}
-				{timer4}
+				{timer2}
+				{timer3}
 			</h1>
 		</div>
 	);
